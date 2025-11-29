a8ea8b296ca7b97e5eda959f2b0661d4bcc4e74f
Felipe Dinnouti <fdinnouti@gmail.com>
2025-11-29 11:37:12 -0300

    fix: Graceful shutdown (wrong commit)

---
+++ src/threadpool/mod.rs
@@ -0,0 +1,82 @@
+use std::sync::{Arc, Mutex, mpsc};
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Option<mpsc::Sender<Job>>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        // Arc + Mutex so multiple workers can use receiver safely
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender: Some(sender) }
+    }
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        
+        // We borrow the Option and unwrap the reference:
+        if let Some(sender) = &self.sender {
+            sender.send(job).unwrap();
+        } else {
+            panic!("ThreadPool has shut down — cannot execute more jobs");
+        }
+    }
+}
+
+impl Drop for ThreadPool { 
+    fn drop(&mut self) {
+        println!("Threadpool dropping, closing sender...");
+
+        drop(self.sender.take());
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap(); 
+            }
+        }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: Option<std::thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = std::thread::spawn(move || loop {
+            match receiver.lock().unwrap().recv() {
+                Ok(job) => {
+                    println!("Worker {id} got a job, executing.");
+                    job()
+                },
+                Err(_) => {
+                    println!("Worker shutting down.");
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
